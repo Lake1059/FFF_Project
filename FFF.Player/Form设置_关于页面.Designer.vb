a87046e2383f55a26136832bcf4232d5063f0340
1059 Studio <vb1059@outlook.com>
2026-09-24 09:54:16 +0800

    2026/9/24-3

--- FFF.Player/Form设置_关于页面.Designer.vb
+++ FFF.Player/Form设置_关于页面.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Form设置_关于页面
     Private Sub InitializeComponent()
         ModernPanel1 = New LakeUI.ModernPanel()
         HtmlColorLabel1 = New LakeUI.HtmlColorLabel()
+        HtmlColorLabel2 = New LakeUI.HtmlColorLabel()
+        HtmlColorLabel3 = New LakeUI.HtmlColorLabel()
         ModernPanel1.SuspendLayout()
         SuspendLayout()
         ' 
@@ -31,6 +33,8 @@ Partial Class Form设置_关于页面
         ' 
         ModernPanel1.BackColor1 = Color.FromArgb(CByte(24), CByte(24), CByte(24))
         ModernPanel1.BorderSize = 0
+        ModernPanel1.Controls.Add(HtmlColorLabel3)
+        ModernPanel1.Controls.Add(HtmlColorLabel2)
         ModernPanel1.Controls.Add(HtmlColorLabel1)
         ModernPanel1.Dock = DockStyle.Fill
         ModernPanel1.Location = New Point(0, 0)
@@ -53,6 +57,36 @@ Partial Class Form设置_关于页面
         HtmlColorLabel1.TabIndex = 19
         HtmlColorLabel1.Text = "<span style=""font-size:13; color:Silver"">3FP 极简本地播放器</span><br>确保将 Shared FFmpeg 的所有 dll 放在程序目录或环境变量中才可正常播放<br>理论上编码是否支持取决于该版本的 FFmpeg 能否支持"
         ' 
+        ' HtmlColorLabel2
+        ' 
+        HtmlColorLabel2.AutoSize = True
+        HtmlColorLabel2.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        HtmlColorLabel2.Dock = DockStyle.Top
+        HtmlColorLabel2.ForeColor = Color.FromArgb(CByte(120), CByte(255), CByte(255), CByte(255))
+        HtmlColorLabel2.LineSpacing = 3
+        HtmlColorLabel2.Location = New Point(10, 83)
+        HtmlColorLabel2.Margin = New Padding(2)
+        HtmlColorLabel2.Name = "HtmlColorLabel2"
+        HtmlColorLabel2.Padding = New Padding(0, 20, 0, 0)
+        HtmlColorLabel2.Size = New Size(550, 103)
+        HtmlColorLabel2.TabIndex = 20
+        HtmlColorLabel2.Text = "<span style=""font-size:13; color:Silver"">核心用途和发展方向</span><br>3FP 最初只是为了向 3FUI 提供帧级精确的剪辑区间时间戳<br>后续开发不是为了对标市面上任何播放器产品，而是完全技术测试方向<br>因此不要提任何舒适度的需求，也不要幻想任何花里胡哨的功能"
+        ' 
+        ' HtmlColorLabel3
+        ' 
+        HtmlColorLabel3.AutoSize = True
+        HtmlColorLabel3.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        HtmlColorLabel3.Dock = DockStyle.Top
+        HtmlColorLabel3.ForeColor = Color.FromArgb(CByte(120), CByte(255), CByte(255), CByte(255))
+        HtmlColorLabel3.LineSpacing = 3
+        HtmlColorLabel3.Location = New Point(10, 186)
+        HtmlColorLabel3.Margin = New Padding(2)
+        HtmlColorLabel3.Name = "HtmlColorLabel3"
+        HtmlColorLabel3.Padding = New Padding(0, 20, 0, 0)
+        HtmlColorLabel3.Size = New Size(550, 83)
+        HtmlColorLabel3.TabIndex = 21
+        HtmlColorLabel3.Text = "<span style=""font-size:13; color:Silver"">技术声明</span><br>3FP 不是套壳播放器，其渲染管线完全独立搭建<br>不会支持任何外部渲染器和特效插件"
+        ' 
         ' Form设置_关于页面
         ' 
         AutoScaleDimensions = New SizeF(96F, 96F)
@@ -71,4 +105,6 @@ Partial Class Form设置_关于页面
 
     Friend WithEvents ModernPanel1 As LakeUI.ModernPanel
     Friend WithEvents HtmlColorLabel1 As LakeUI.HtmlColorLabel
+    Friend WithEvents HtmlColorLabel3 As LakeUI.HtmlColorLabel
+    Friend WithEvents HtmlColorLabel2 As LakeUI.HtmlColorLabel
 End Class
